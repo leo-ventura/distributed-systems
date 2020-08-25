9dda1530c638145ab6eda4687d58ded5407a9353
Leonardo Ventura <leonardo.venturacl@gmail.com>
2020-08-25 13:07:09 -0300

    Adiciona shebang a server.py

--- 01-echo/server.py
+++ 01-echo/server.py
@@ -1,28 +1,11 @@
+#!/bin/python3
 import argparse
 import logging
 
+from util import parseArguments, setupLogging
 from socket import socket, AF_INET, SOCK_STREAM
 
-LOG_FORMAT = "[%(asctime)s | %(levelname)s]: %(message)s"
-
-def setupLogging():
-    logging.basicConfig(format=LOG_FORMAT, level=logging.DEBUG)
-
-def parseArguments():
-    parser = argparse.ArgumentParser(
-        description='Basic echo server implementation')
-    parser.add_argument('--host', type=str, default='localhost',
-                        help='Host', dest='host')
-    parser.add_argument('-P', '--port', type=int, default=5000,
-                        help='Port', dest='port')
-
-    args = parser.parse_args()
-    host = args.host
-    port = args.port
-
-    return host, port
-
-def main():
+def serve():
     setupLogging()
     listenerAddress = parseArguments()
 
@@ -64,4 +47,4 @@ def main():
     logging.info('All connections closed, shutting down server')
 
 if __name__ == '__main__':
-    main()
+    serve()
