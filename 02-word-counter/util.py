24a1821980f2f7e94176e694227bef37acc4a315
Leonardo Ventura <leonardo.venturacl@gmail.com>
2020-09-08 21:25:40 -0300

    [02] Adiciona utilitario de cores

--- 02-word-counter/util.py
+++ 02-word-counter/util.py
@@ -1,6 +1,16 @@
 import argparse
 import logging
 
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 LOG_FORMAT = "[%(asctime)s | %(levelname)s]: %(message)s"
 
 def setupLogging():
