f9cc4da4ebfe018b2b97bd5fbb3d34b4dfbe5733
Leonardo Ventura <leonardo.venturacl@gmail.com>
2020-10-01 11:44:13 -0300

    [chat] add abstract listener socket

---
+++ chat/sockets_wrappers/listener_socket.py
@@ -0,0 +1,35 @@
+import logging
+
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+
+class ListenerSocket:
+    def __init__(self, address=('', 0), max_connections=5):
+        """ Binding to port 0 lets the SO choses a random port for us """
+        self._sock = self._initSocket(address, max_connections)
+
+    def fileno(self):
+        return self._sock.fileno()
+
+    def close(self):
+        self._sock.close()
+
+    def get_address(self):
+        return self._sock.getsockname()
+
+    def _initSocket(self, address, max_connections):
+        # set up socket descriptor
+        sock = socket(AF_INET, SOCK_STREAM)
+
+        # bind connection to host and port given as parameters
+        logging.debug(f'Binding connection on: {address}')
+        sock.bind(address)
+
+        # set up listener for new connections, max of max_connections simultaneous connection
+        logging.debug(f'Listening on {sock.getsockname()}...')
+        sock.listen(max_connections)
+
+        # sock.setblocking(False)
+        # set timeout to 30 seconds
+        # sock.settimeout(30)
+        return sock
