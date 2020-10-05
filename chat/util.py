60da3b8e8101485615a1e639e45b188090e6134a
Leonardo Ventura <leonardo.venturacl@gmail.com>
2020-10-05 13:00:58 -0300

    Add colored print utility

--- chat/util.py
+++ chat/util.py
@@ -5,6 +5,7 @@ class colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
+    CYAN = '\033[96m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
@@ -17,17 +18,24 @@ def shell_print():
     print("> ", end="", flush=True)
 
 def print_message_chat():
-    print("#>", end="", flush=True)
+    print(f"{colors.CYAN}[Você] #>{colors.ENDC}", end="", flush=True)
 
 def print_received_message(nickname, message):
     print(f'{colors.OKBLUE}[@{nickname}]: {message}{colors.ENDC}')
     shell_print()
 
-def print_nicknames(nicknames):
+def print_colored(color, string):
+    print(f'{color}{string}{colors.ENDC}')
+
+def print_nicknames(nicknames, self_nickname=""):
     if nicknames:
         index = 1
-        for key in nicknames:
-            print(f'[{index}]: @{key}')
+        for nickname in nicknames:
+            formatted_output = f'[{index}]: @{nickname}'
+            if nickname == self_nickname:
+                print_colored(colors.CYAN, formatted_output)
+            else:
+                print(formatted_output)
             index += 1
     else:
         print(f"{colors.WARNING}Nenhum usuário ativo{colors.ENDC}")
